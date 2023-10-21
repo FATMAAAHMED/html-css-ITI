d95bf912d0bab8e959ccbf8a4458c2f3a1f774a4
FATMAAAHMED <fatmaahmedmohamed09@gmai.com>
2023-10-21 13:48:53 +0300

    html&css

---
+++ lab2html5&css3/index.js
@@ -0,0 +1,9 @@
+var uNAME = document.getElementsByClassName("name")[0];
+function displayName() {
+  uNAME.style.opacity = 1;
+  uNAME.style.width = "200px";
+}
+function hideElement() {
+  uNAME.style.opacity = 0;
+  uNAME.style.width = "50px";
+}
